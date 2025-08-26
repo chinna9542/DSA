cfac9e64b2714d40c11877415768af471b59666e
PADIGAPATI VENKATA LAKSHMI NARAYANA REDDY <plakshminarayana9010@gmail.com>
2025-08-26 21:53:29 +0530

    Time: 2 ms (94.98%) | Memory: 49.9 MB (96.21%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.java
+++ 485-max-consecutive-ones/max-consecutive-ones.java
@@ -1,7 +1,7 @@
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
-        int c=0;
         int max=Integer.MIN_VALUE;
+        int c=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]==1) c++;
             else{
@@ -9,7 +9,7 @@ class Solution {
                 c=0;
             }
         }
-        max=Math.max(c,max);
+        max=Math.max(max,c);
         return max;
     }
 }
