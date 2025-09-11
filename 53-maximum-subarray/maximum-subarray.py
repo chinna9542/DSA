43a1f8bdd5af0ffe9717d6c4da8fb907b7d434c2
PADIGAPATI VENKATA LAKSHMI NARAYANA REDDY <plakshminarayana9010@gmail.com>
2025-09-11 07:58:30 +0530

    Time: 98 ms (12.90%) | Memory: 32.9 MB (21.89%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        cur=nums[0]
+        max_sum=nums[0]
+        for i in range(1,len(nums)):
+            cur = max(cur+nums[i],nums[i])
+            max_sum=max(max_sum,cur)
+        return max_sum
