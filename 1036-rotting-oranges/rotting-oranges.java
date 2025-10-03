133b0256b83201cdb0398c1c5b7631ccbb566257
PADIGAPATI VENKATA LAKSHMI NARAYANA REDDY <plakshminarayana9010@gmail.com>
2025-10-03 21:13:05 +0530

    Time: 2 ms (65.96%) | Memory: 42.9 MB (21.33%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,9 +1,9 @@
 class Solution {
-    class pair{
+    static class Pair{
         int row;
         int col;
         int tm;
-        pair(int row,int col,int tm){
+        Pair(int row,int col,int tm){
             this.row=row;
             this.col=col;
             this.tm=tm;
@@ -13,40 +13,40 @@ class Solution {
         int m=grid.length;
         int n=grid[0].length;
         int vis[][]=new int[m][n];
-        Queue<pair> q=new LinkedList<>();
-        int cnt=0;
+        int fresh=0;
+        Queue<Pair> q=new LinkedList<>();
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==2){
                     vis[i][j]=2;
-                    q.add(new pair(i,j,0));
+                    q.add(new Pair(i,j,0));
                 }else{
                     vis[i][j]=0;
                 }
-                if(grid[i][j]==1) cnt++;
+                if(grid[i][j]==1) fresh++;
             }
         }
-        int cn=0;
+        int rottenCount=0;
         int a[]={-1,0,+1,0};
         int b[]={0,+1,0,-1};
-        int t=0;
+        int time=0;
         while(!q.isEmpty()){
-            pair cur=q.poll();
+            Pair cur=q.poll();
             int r=cur.row;
             int c=cur.col;
             int tm=cur.tm;
-            t=Math.max(t,tm);
+            time=Math.max(time,tm);
             for(int i=0;i<4;i++){
-                int nr=r+a[i];
-                int nc=c+b[i];
-                if(nr>=0&&nr<m&&nc>=0&&nc<n&&vis[nr][nc]==0&&grid[nr][nc]==1){
-                    q.add(new pair(nr,nc,tm+1));
-                    vis[nr][nc]=2;
-                    cn++;
+                int currow=r+a[i];
+                int curcol=c+b[i];
+                if(currow>=0&&currow<m&&curcol>=0&&curcol<n&&vis[currow][curcol]==0&&grid[currow][curcol]==1){
+                    q.add(new Pair(currow,curcol,tm+1));
+                    vis[currow][curcol]=2;
+                    rottenCount++;
                 }
             }
         }
-        if(cn!=cnt) return -1;
-        return t;
+        if(fresh!=rottenCount) return -1;
+        else return time;
     }
 }
