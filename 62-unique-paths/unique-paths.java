527ec038cde307aa14b93801c370e922ce860047
PADIGAPATI VENKATA LAKSHMI NARAYANA REDDY <plakshminarayana9010@gmail.com>
2025-11-24 22:46:25 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (30.43%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,11 +1,16 @@
 class Solution {
+    static int helper(int i,int j,int dp[][]){
+        if(i==0||j==0) return 1;
+        if(dp[i][j]!=-1) return dp[i][j];
+        return dp[i][j]=helper(i-1,j,dp)+helper(i,j-1,dp);
+    }
     public int uniquePaths(int m, int n) {
-        int N=m+n-2;
-        int R=m-1;
-        long res=1;
-        for(int i=1;i<=R;i++){
-            res=res*(N-R+i)/i;
+        int dp[][]=new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                dp[i][j]=-1;
+            }
         }
-        return (int)res;
+        return helper(m-1,n-1,dp);
     }
 }
