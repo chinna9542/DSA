b7aedd005c1bda93802f4593e997b84670cace15
PADIGAPATI VENKATA LAKSHMI NARAYANA REDDY <plakshminarayana9010@gmail.com>
2025-12-21 11:13:59 +0530

    Time: 1 ms (62.79%) | Memory: 43.4 MB (22.48%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,17 +1,13 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
+        if(strs==null||strs.length==0) return "";
         Arrays.sort(strs);
+        StringBuilder sb=new StringBuilder();
+        String first=strs[0];
+        String last=strs[strs.length-1];
+        int l=Math.min(first.length(),last.length());
         int i=0;
-        String s="";
-        String s1=strs[0];
-        String s2=strs[strs.length-1];
-        while(i<s1.length()){
-            if(s1.charAt(i)!=s2.charAt(i)){
-                break;
-            }
-            s=s+s1.charAt(i);
-            i++;
-        }
-        return s;
+        while(i<l&&first.charAt(i)==last.charAt(i)) i++;
+        return first.substring(0,i);
     }
 }
