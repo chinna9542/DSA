44108bebc0222b62a41c92b757195441d3c7437f
PADIGAPATI VENKATA LAKSHMI NARAYANA REDDY <plakshminarayana9010@gmail.com>
2025-12-21 10:55:27 +0530

    Time: 3 ms (98.78%) | Memory: 44.5 MB (41.81%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String reverseWords(String s) {
+        int n=s.length();
+        StringBuilder sb=new StringBuilder();
+        int i=n-1;
+        while(i>=0){
+            while(i>=0&&s.charAt(i)==' ') i--;
+            if(i<0) break;
+            int j=i;
+            while(j>=0&&s.charAt(j)!=' ') j--;
+            sb.append(s.substring(j+1,i+1)).append(' ');
+            i=j-1;
+        }
+        return sb.toString().trim();
+    }
+}
